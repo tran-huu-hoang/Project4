e9a821e2b0562d69405064a40f6c3dc2fda4343f
aaa <mhung4011@gmail.com>
2024-06-28 15:38:49 +0700

    create api rate, favourite product

---
+++ Project4/Project4/Repositories/RateRepository/RateRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project4.Models;
+
+namespace Project4.Repositories.RateRepository
+{
+    public class RateRepository : IRateRepository
+    {
+        private readonly MockProject4DbContext _context;
+
+        public RateRepository(MockProject4DbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Rate>> GetRates()
+        {
+            return await _context.Rates.ToListAsync();
+        }
+        public async Task<Rate> GetRate(int id)
+        {
+            return await _context.Rates.FindAsync(id); ;
+        }
+
+        public async Task<List<Rate>> PostRate(Rate rate)
+        {
+            _context.Rates.Add(rate);
+            await _context.SaveChangesAsync();
+
+            return await _context.Rates.ToListAsync();
+        }
+
+        public async Task<List<Rate>> PutRate(int id, Rate rate)
+        {
+            _context.Rates.Update(rate);
+
+            await _context.SaveChangesAsync();
+
+            return await _context.Rates.ToListAsync();
+        }
+        public async Task<List<Rate>> DeleteRate(int id)
+        {
+            var rate = await _context.Rates.FindAsync(id);
+
+            _context.Rates.Remove(rate);
+            await _context.SaveChangesAsync();
+
+            return await _context.Rates.ToListAsync();
+        }
+    }
+}
